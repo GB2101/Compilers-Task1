b9917771eac273f8dd7b181fc8362f9b76c8ac8b
Gabriel Braz <gabrielbrazcs@gmail.com>
2023-04-04 12:41:43 -0300

    Created functions for helping users

--- main.cpp
+++ main.cpp
@@ -2,8 +2,28 @@
 
 using std::cin, std::cout, std::endl, std::string;
 
-int main(int number, char* args[]) {
-	cout << number << endl;
-	cout << args[0] << endl;
-	cout << args[1] << endl;
+bool Display(string& command) {
+	if (command == "--version" || command == "-v") {
+		cout << "version 1.0.0.0" << endl;
+		return false;
+	}
+	if (command == "--help" || command == "-h") {
+		cout << "calculate" << endl;
+		cout << endl;
+		cout << "> calculate <file>" << endl;
+		cout << "> calculate --help | -h" << endl;
+		cout << "> calculate --version | -v" << endl;
+		return false;
+	}
+	return true;
+}
+
+int main(int number, const char* args[]) {
+	if (number != 2) {
+		cout << "Invalid number of arguments, please use --help (-h) for more informations." << endl;
+		return 1;
+	}
+
+	string command = args[1];
+	bool calculate = Display(command);
 }
