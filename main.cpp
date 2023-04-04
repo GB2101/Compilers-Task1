5b8ebd1e330c2d7977214e17f21910c486f85996
Gabriel Braz <gabrielbrazcs@gmail.com>
2023-04-04 12:29:25 -0300

    Initial Commit

---
+++ main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+using std::cin, std::cout, std::endl, std::string;
+
+int main(int number, char* args[]) {
+	cout << number << endl;
+	cout << args[0] << endl;
+	cout << args[1] << endl;
+}
